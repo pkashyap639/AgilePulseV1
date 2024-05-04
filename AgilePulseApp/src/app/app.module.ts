37ee26ada37cb00d5b19b6ea25a034b022cbdf78
pkashyap639 <pkashyap_639@hotmail.com>
2024-05-03 22:00:56 -0400

    SignUp Form Frontend

--- AgilePulseApp/src/app/app.module.ts
+++ AgilePulseApp/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignInFormComponent } from './Components/AuthForms/sign-in-form/sign-in-form.component';
 import { SignUpFormComponent } from './Components/AuthForms/sign-up-form/sign-up-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { SignUpFormComponent } from './Components/AuthForms/sign-up-form/sign-up
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
