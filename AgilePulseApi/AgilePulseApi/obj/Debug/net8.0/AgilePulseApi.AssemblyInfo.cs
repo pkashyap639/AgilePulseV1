4064fe8e3722c913400b9f5768b185fcff3030d1
pkashyap639 <pkashyap_639@hotmail.com>
2024-05-05 18:43:58 -0400

    Main Sidebar

--- AgilePulseApi/AgilePulseApi/obj/Debug/net8.0/AgilePulseApi.AssemblyInfo.cs
+++ AgilePulseApi/AgilePulseApi/obj/Debug/net8.0/AgilePulseApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgilePulseApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2aa24f6ab72e49845790c42144d8e834fa0a669c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16d599c3c556384ad913974e6a39a84f503b48bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgilePulseApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgilePulseApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
